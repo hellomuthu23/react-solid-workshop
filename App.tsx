7f26589cbf1093c2cf1f58b174d1ae3ed961090d
hellomuthu23 <59234994+hellomuthu23@users.noreply.github.com>
2023-02-01 13:32:14 +0000

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to React Solid Principle Workshop</h1>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-solid-workshop
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-hnxa5r)
